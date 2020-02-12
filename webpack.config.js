c4a8a0f6a65f97b6b233d088c6343a70094a5b5e
Eva HOUZARD <ehouzard@student.42.fr>
2020-02-12 18:08:12 +0100

    package.json updated + tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/client/index.js',
+  entry: "./src/client/index.js",
 
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js"
   },
 
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query:{
-        presets: ["es2015", "react", "stage-0"]
+      loaders: 
+      [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["env", "react", "stage-0"]
+        }
+      }],
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|gif|mp3)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name][hash].[ext]"
+            }
+          }
+        ]
       }
-    }]
+    ]
   }
 };
