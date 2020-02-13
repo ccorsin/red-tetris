3d92ff2d0c827379c68778b75c89f4150be23159
Eva HOUZARD <ehouzard@student.42.fr>
2020-02-13 19:58:21 +0100

    wip on router

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,101 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import { StyledApp } from '../components/styles/StyledApp';
 
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  // useRouteMatch
+} from "react-router-dom";
+
+import history from "../history";
+
 import { connect } from 'react-redux'
 import io from 'socket.io-client'
-// import Playground from "./Playground"
-import Tetris from "../components/Tetris";
+import Playground from "./Playground";
+import Menu from "../components/Menu";
+
 const socket = io('http://0.0.0.0:3004');
 
 const App = ({message}) => {
-  
-  // point d'entree
-  // gerer l'attribution des room
-  // component recupere le nom et generate newGame ou input roomName
-  // socket - gestion attribution room et playerid pour redirection vers Playground
-  /*
-    
-  useEffect(() => {
-    socket.emit('newRoom', username); // generate newRoom
-    socket.on('goPlay', function (goPlay) {
-      // URL redirection to Playground
-      // goPlay.roomNb
-    })
 
-  */
+  // Set menu value
+  const [player, setPlayer] = useState("PLAYER");
+  const [roomNb, setRoomNb] = useState("");
+  const [go, setGo] = useState(false);
+  const [path, setPath] = useState("/");
 
-
-  // SI params dans l'URL => Playground
-  console.log(window.location.href);
-  const reg = /(#[\d]+)(\[\w+\])/;
-  const params = reg.exec(window.location.href);
-  console.log(params);
-  
-  let room;
-  let username;
-  let commands = "";
-
-  // ?
-  const [playerCount, setPlayerCount] = useState(1);
-
-  if (params !== null) {
-    room = params[1].substring(1);
-    username = params[2].slice(1,-1);
-    
-    const [gameLeader, setGameLeader] = useState(username);
-    const [runningState, setRunningState] = useState(false);
-    const startGame = () => {
-      socket.emit('start', room)
-    }
-    const endGame = () => {
-      socket.emit('end', room)
-    }
-    const isLeader = username == gameLeader
-  
-    if (isLeader && !runningState) {
-      commands = <button onClick={startGame}>START</button>;
-    } else if (isLeader && runningState) {
-      commands = <button onClick={endGame}>STOP</button>;
-    } else if (!runningState) {
-      commands = <h2>Wait for {gameLeader} to start the game !</h2>
-    } else {
-      commands = <h2>Game is ON !</h2>
-    }
-
-    useEffect(() => {
-      socket.emit('room', room, username);
-      socket.on('message', function(message) {
-        alert(message);
-      })
-      socket.on('players_game', function(count, leader) {
-        setPlayerCount(count);
-        setGameLeader(leader)
-      });
-      socket.on('toggle_game', function(isRunning) {
-        setRunningState(isRunning)
-      });
-    }, [])
+  // send ids to socket
+  if (go) {
+    socket.emit("room", roomNb, player);
+    setPath("/tetris/#"+roomNb+"["+player+"]");
+    setPlayer("PLAYER");
+    setRoomNb("");
+    setGo(false);
+    history.push(path);
   }
 
-  
+  let room = "";
+  let username = "";
+  useEffect(() => {
+    console.log(path);
+    // SI params dans l'URL => Playground
+    const reg = /(\/tetris\/#[\d]+)(\[\w+\])/;
+    const params = reg.exec(window.location.href);
+    if (params !== null) {
+      room = params[1].slice(9);
+      username = params[2].slice(1, -1);
 
-  //
-
+      // let match = useRouteMatch("/tetris/");
+      // return <Playground socket={socket} room={room} username={username} />;
+    }
+  }, []);
+ 
   return (
     <StyledApp>
-      {status}
-      {commands}
-      {params !== null ? (
-        <div>
-          <h1>Currently {playerCount} players in the game.</h1>
-          <Tetris />
-          {/* <Playground 
-          /> */}
-        </div>
-        ) : (
-          <div>
-          </div>
-        ) }
+      <Router history={history}>
+        <Route path="/tetris/">
+          <Playground socket={socket} room={room} username={username} />
+        </Route>
+        <Route exact path="/">
+          <Menu playerName={setPlayer} roomNb={setRoomNb} goToRoom={setGo} />
+        </Route>
+      </Router>
     </StyledApp>
   );
 }
