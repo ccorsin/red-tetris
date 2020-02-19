8137cb76ffcc719a9f1a6fa96450b60a2249ccf2
Eva HOUZARD <ehouzard@student.42.fr>
2020-02-19 10:10:40 +0100

    prepare socket

--- src/client/components/Stage.js
+++ src/client/components/Stage.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { StyledStage } from './styles/StyledStage';
+import { StyledStage, StyledRow } from './styles/StyledStage';
 
 import Cell from './Cell';
 
 const Stage = ({ stage }) => (
   <StyledStage width={stage[0].length} height={stage.length}>
-    {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
+    {stage.map((row, y) => (
+      <StyledRow className="row" key={y}>
+        {row.map((cell, x) => <Cell key={x} type={cell[0]} />)}
+      </StyledRow>
+    ))}
   </StyledStage>
+
 );
 
 export default Stage;
