a621f35c2aa0721a5ad84efa4893711de3881e69
Celia CORSIN <ccorsin@student.42.fr>
2020-02-19 11:01:47 +0100

    Add key var in PLayer

--- src/server/helpers/player.js
+++ src/server/helpers/player.js
@@ -2,5 +2,30 @@ export default class Player {
     constructor (name, id) {
         this.name = name
         this.id = id
+        this.line = 0
+        this.round = 0
+        this.loser = false
+    }
+
+    add_round() {
+        this.round += 1;
+    }
+
+    add_line(n) {
+        this.line += n;
+        if (this.line > 19) {
+            this.line = 19;
+        }
+    }
+
+    remove_line(n) {
+        this.line -= n;
+        if (this.line < 0) {
+            this.line = 0;
+        }
+    }
+
+    lose() {
+        this.loser = true
     }
 }
--- test/player.test.js
+++ test/player.test.js
@@ -7,7 +7,55 @@ import Player from "../src/server/helpers/player"
     it('should update player with given values', () => {
         expect(player).toEqual({
             name: 'name',
-            id: 42
+            id: 42,
+            line: 0,
+            round: 0,
+            loser: false
+        })
+    })
+
+    it('should increment player round', () => {
+        player.add_round();
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 0,
+            round: 1,
+            loser: false
+        })
+    })
+
+    it('should add 1 line to player', () => {
+        player.add_line(1);
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 1,
+            round: 0,
+            loser: false
+        })
+    })
+
+    it('should remove 1 line to player', () => {
+        player.add_line(5);
+        player.remove_line(2);
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 3,
+            round: 0,
+            loser: false
+        })
+    })
+
+    it('should be a loser', () => {
+        player.lose();
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 0,
+            round: 0,
+            loser: true
         })
     })
  })
