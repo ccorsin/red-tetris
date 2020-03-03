d54e6bc5e9262826d380327efe4239ba44f53ce5
Celia CORSIN <ccorsin@student.42.fr>
2020-03-03 16:21:58 +0100

    Fix - refill before end - 1

--- client/src/hooks/usePlayer.js
+++ client/src/hooks/usePlayer.js
@@ -49,7 +49,6 @@ export const usePlayer = () => {
       };
 
       const resetPlayer = useCallback((currentPlayer, tetriminos) => {
-        const round = store.getState().sock.currentPlayer.round;
         setPlayer({
           pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
           tetromino: tetriminos[currentPlayer.round].shape,
