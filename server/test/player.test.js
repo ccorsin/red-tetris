be635759c367b2c0c20d527f2353dcd660f58265
Celia CORSIN <ccorsin@student.42.fr>
2020-03-05 17:09:10 +0100

    Test server - Player - OK

--- server/test/player.test.js
+++ server/test/player.test.js
@@ -1,4 +1,4 @@
-const Player = require('../src/server/helpers/player');
+const Player = require('../helpers/player');
 
  describe('Player class is made to represent a player, store its name and socket id', () => {
     let player;
@@ -10,7 +10,9 @@ const Player = require('../src/server/helpers/player');
             id: 42,
             line: 0,
             round: 0,
-            loser: false
+            loser: false,
+            color: player.color,
+            freeze: 0
         });
     });
 
@@ -21,30 +23,50 @@ const Player = require('../src/server/helpers/player');
             id: 42,
             line: 0,
             round: 1,
-            loser: false
+            loser: false,
+            color: player.color,
+            freeze: 0
         });
     });
 
     it('should add 1 line to player', () => {
-        player.add_line(1);
+        player.add_line();
         expect(player).toEqual({
             name: 'name',
             id: 42,
             line: 1,
             round: 0,
-            loser: false
+            loser: false,
+            color: player.color,
+            freeze: 0
+        });
+    });
+
+    it('should set 5 line in reverse order to player', () => {
+        player.set_line(5);
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 14,
+            round: 0,
+            loser: false,
+            color: player.color,
+            freeze: 0
         });
     });
 
     it('should remove 1 line to player', () => {
-        player.add_line(5);
-        player.remove_line(2);
+        player.add_line();
+        player.add_line();
+        player.remove_line();
         expect(player).toEqual({
             name: 'name',
             id: 42,
-            line: 3,
+            line: 1,
             round: 0,
-            loser: false
+            loser: false,
+            color: player.color,
+            freeze: 0
         });
     });
 
@@ -55,7 +77,63 @@ const Player = require('../src/server/helpers/player');
             id: 42,
             line: 0,
             round: 0,
-            loser: true
+            loser: true,
+            color: player.color,
+            freeze: 0
+        });
+    });
+
+    it('should freeze 1 line to player', () => {
+        player.add_freeze();
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 0,
+            round: 0,
+            loser: false,
+            color: player.color,
+            freeze: 1
+        });
+    });
+
+    it('should become a winner', () => {
+        player.win();
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 0,
+            round: 0,
+            loser: false,
+            color: player.color,
+            freeze: 0,
+            winner: true
+        });
+    });
+
+    it('should reset a player', () => {
+        player.win();
+        player.add_round();
+        player.add_line();
+        player.add_freeze();
+        const newPlayer = player.reset_player();
+        expect(player).toEqual({
+            name: 'name',
+            id: 42,
+            line: 1,
+            round: 1,
+            loser: false,
+            color: player.color,
+            freeze: 1,
+            winner: true
+        });
+        expect(newPlayer).toEqual({
+            name: 'name',
+            id: 42,
+            line: 0,
+            round: 0,
+            loser: false,
+            color: newPlayer.color,
+            freeze: 0
         });
     });
  });
