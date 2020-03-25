f44e871f1ac362c55744895bdf022c3efd1b4f84
Eva Houzard <evah_@hotmail.fr>
2020-03-25 17:08:38 +0100

    cleand playground test

--- client/src/components/test/Playground.test.js
+++ client/src/components/test/Playground.test.js
@@ -4,7 +4,7 @@ import { Router } from 'react-router-dom';
 import Playground from '../Playground';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
@@ -16,46 +16,45 @@ import tetriminosReducer from "../../reducers/tetriminos";
 
 const initialState = {};
 const rootReducer = combineReducers({
-    sock: socketReducer,
-    tetriminos: tetriminosReducer
+  sock: socketReducer,
+  tetriminos: tetriminosReducer
 });
 const middleware = [
-    ioSocketMiddleWare,
-    thunk,
-    createLogger()
+  ioSocketMiddleWare,
+  thunk,
+  createLogger()
 ];
 
 export const store = createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(...middleware))
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware))
 
 describe('<Playground/> Component', () => {
-  it('rendering correctly with no shapes', () => {
-    const div = document.createElement('div')
+  it('rendering and redirect cause no params', () => {
     const setIsAlert = jest.fn();
     const setIsRunning = jest.fn();
     const setAlertMessage = jest.fn();
-    // const historyMock = { push: jest.fn(), location: {}, listen: jest.fn() };
-    // ReactDom.render(
-    //   <Provider store={store}>
-    //     <Router history={historyMock}>
-    //       <Playground setIsAlert={""} setAlertMessage={""} setIsRunning={""}/>
-    //     </Router>
-    //   </Provider>, div)
-    // ReactDom.unmountComponentAtNode(div)
     const playground = mount(
       <Provider store={store}>
         <MemoryRouter initialEntries={['/room/#0[op]']}>
-          <Playground path='/room' setIsAlert={setIsAlert} setIsRunning={setIsRunning} setAlertMessage={setAlertMessage}/>
+          <Playground setIsAlert={setIsAlert} setIsRunning={setIsRunning} setAlertMessage={setAlertMessage} />
         </MemoryRouter>
       </Provider>
     )
-    console.log(playground.debug())
-    expect(playground.find('Header').length).toEqual(0);
-    // expect(setIsAlert).not.toHaveBeenCalled();
-    // expect(setIsRunning).not.toHaveBeenCalled();
-    // expect(setAlertMessage).not.toHaveBeenCalled();
-
+    expect(playground.find('Redirect').length).toEqual(1);
+  });
+  it('rendering with no crash', () => {
+    const setIsAlert = jest.fn();
+    const setIsRunning = jest.fn();
+    const setAlertMessage = jest.fn();
+    const playground = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/room/#0[op]']}>
+          <Playground setIsAlert={setIsAlert} setIsRunning={setIsRunning} setAlertMessage={setAlertMessage} />
+        </MemoryRouter>
+      </Provider>
+    )
+    expect(playground.find('Playground').length).toEqual(1);
   });
 })
